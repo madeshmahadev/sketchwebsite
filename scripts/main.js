44b8b10e2c7dd44957a80509a27e330de5adad24
madeshmahadev <madeshmaddy9@gmail.com>
2020-09-28 02:49:36 +0530

    sketch site

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+window.APP = window.APP || {};
+APP.VARS = {};
+APP.BASE_URL = 'index.html';
+APP.THEME_URL = 'vendor/content/themes/twig-starter/index.html';
+APP.AJAX_URL = 'vendor/admin/admin-ajax.html';
+APP.REST_URL = 'vendor/json/index.html';
+APP.NONCE = '12ad56a67a';
+APP.ENVIROMENT = '';
+APP.PAGES = '[{"id":9,"title":"Contact","slug":"contact"},{"id":2,"title":"Home","slug":"sample-page"},{"id":101,"title":"Works","slug":"works"}]'
